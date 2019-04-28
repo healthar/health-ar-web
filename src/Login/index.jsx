5ef41ca8a136b007e8412076f6ade252781443c2
serpient <littlegangwolf@gmail.com>
2019-04-28 16:47:33 -0700

    project setup part 2
    - add react-router dom
    - change .css to scss
    - add css reset
    - create starter Login component

---
+++ src/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.scss';
+
+const Login = () => {
+    return (
+        <section className='login-container'>
+            <section className='login--left'>
+                {/* form */}
+            </section>
+        </section>
+    )
+}
+
+export default Login;
